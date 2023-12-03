6e554163f1340bed2f83ef36ceb9e217a149d8b1
Eduardo <edu.almeida1@gmail.com>
2023-12-03 15:08:37 +0000

    Update dockerbuildstep.sh

--- build/dockerbuildstep.sh
+++ build/dockerbuildstep.sh
@@ -1,4 +1,4 @@
 docker network create tulip-net || true
 docker build -t angular-app-image .
 docker rm -f test-angular || true
-docker run --name test-angular --net tulip-net -d -p 80:80  angular-app-image
+docker run --name test-angular --net tulip-net -d -p 82:80  angular-app-image
